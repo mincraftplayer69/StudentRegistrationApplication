ffc59e2f176b4083750b1da2a093b9d6a4f45ffb
mincraftplayer69 <james.calderon023@gmail.com>
2024-04-10 13:16:38 +0800

    Add project files.

---
+++ StudentRegistrationApplication/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace StudentRegistrationApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lastNameLabel_calderon = new System.Windows.Forms.Label();
+            this.lastNameInput_calderon = new System.Windows.Forms.TextBox();
+            this.firstNameInput_calderon = new System.Windows.Forms.TextBox();
+            this.firstNameLabel_calderon = new System.Windows.Forms.Label();
+            this.middleNameInput_calderon = new System.Windows.Forms.TextBox();
+            this.middleNameLabel_calderon = new System.Windows.Forms.Label();
+            this.maleSelect_calderon = new System.Windows.Forms.RadioButton();
+            this.femaleSelect_calderon = new System.Windows.Forms.RadioButton();
+            this.genderLabel_calderon = new System.Windows.Forms.Label();
+            this.birthLabel_calderon = new System.Windows.Forms.Label();
+            this.dayComboBox_calderon = new System.Windows.Forms.ComboBox();
+            this.monthComboBox_calderon = new System.Windows.Forms.ComboBox();
+            this.yearComboBox_calderon = new System.Windows.Forms.ComboBox();
+            this.clearButton_calderon = new System.Windows.Forms.Button();
+            this.exitButton_calderon = new System.Windows.Forms.Button();
+            this.registerButton_calderon = new System.Windows.Forms.Button();
+            this.headerTitle_calderon = new System.Windows.Forms.Label();
+            this.progLabel_calderon = new System.Windows.Forms.Label();
+            this.programComboBox_calderon = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lastNameLabel_calderon
+            // 
+            this.lastNameLabel_calderon.AutoSize = true;
+            this.lastNameLabel_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastNameLabel_calderon.Location = new System.Drawing.Point(99, 68);
+            this.lastNameLabel_calderon.Name = "lastNameLabel_calderon";
+            this.lastNameLabel_calderon.Size = new System.Drawing.Size(105, 20);
+            this.lastNameLabel_calderon.TabIndex = 1;
+            this.lastNameLabel_calderon.Text = "Last name *";
+            // 
+            // lastNameInput_calderon
+            // 
+            this.lastNameInput_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lastNameInput_calderon.Location = new System.Drawing.Point(103, 100);
+            this.lastNameInput_calderon.Name = "lastNameInput_calderon";
+            this.lastNameInput_calderon.Size = new System.Drawing.Size(268, 26);
+            this.lastNameInput_calderon.TabIndex = 2;
+            // 
+            // firstNameInput_calderon
+            // 
+            this.firstNameInput_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.firstNameInput_calderon.Location = new System.Drawing.Point(103, 180);
+            this.firstNameInput_calderon.Name = "firstNameInput_calderon";
+            this.firstNameInput_calderon.Size = new System.Drawing.Size(268, 26);
+            this.firstNameInput_calderon.TabIndex = 4;
+            // 
+            // firstNameLabel_calderon
+            // 
+            this.firstNameLabel_calderon.AutoSize = true;
+            this.firstNameLabel_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNameLabel_calderon.Location = new System.Drawing.Point(99, 148);
+            this.firstNameLabel_calderon.Name = "firstNameLabel_calderon";
+            this.firstNameLabel_calderon.Size = new System.Drawing.Size(106, 20);
+            this.firstNameLabel_calderon.TabIndex = 3;
+            this.firstNameLabel_calderon.Text = "First name *";
+            // 
+            // middleNameInput_calderon
+            // 
+            this.middleNameInput_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.middleNameInput_calderon.Location = new System.Drawing.Point(103, 259);
+            this.middleNameInput_calderon.Name = "middleNameInput_calderon";
+            this.middleNameInput_calderon.Size = new System.Drawing.Size(268, 26);
+            this.middleNameInput_calderon.TabIndex = 6;
+            // 
+            // middleNameLabel_calderon
+            // 
+            this.middleNameLabel_calderon.AutoSize = true;
+            this.middleNameLabel_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.middleNameLabel_calderon.Location = new System.Drawing.Point(99, 227);
+            this.middleNameLabel_calderon.Name = "middleNameLabel_calderon";
+            this.middleNameLabel_calderon.Size = new System.Drawing.Size(122, 20);
+            this.middleNameLabel_calderon.TabIndex = 5;
+            this.middleNameLabel_calderon.Text = "Middle name *";
+            // 
+            // maleSelect_calderon
+            // 
+            this.maleSelect_calderon.AutoSize = true;
+            this.maleSelect_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.maleSelect_calderon.Location = new System.Drawing.Point(185, 296);
+            this.maleSelect_calderon.Name = "maleSelect_calderon";
+            this.maleSelect_calderon.Size = new System.Drawing.Size(61, 24);
+            this.maleSelect_calderon.TabIndex = 8;
+            this.maleSelect_calderon.TabStop = true;
+            this.maleSelect_calderon.Text = "Male";
+            this.maleSelect_calderon.UseVisualStyleBackColor = true;
+            // 
+            // femaleSelect_calderon
+            // 
+            this.femaleSelect_calderon.AutoSize = true;
+            this.femaleSelect_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.femaleSelect_calderon.Location = new System.Drawing.Point(257, 296);
+            this.femaleSelect_calderon.Name = "femaleSelect_calderon";
+            this.femaleSelect_calderon.Size = new System.Drawing.Size(80, 24);
+            this.femaleSelect_calderon.TabIndex = 9;
+            this.femaleSelect_calderon.TabStop = true;
+            this.femaleSelect_calderon.Text = "Female";
+            this.femaleSelect_calderon.UseVisualStyleBackColor = true;
+            // 
+            // genderLabel_calderon
+            // 
+            this.genderLabel_calderon.AutoSize = true;
+            this.genderLabel_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genderLabel_calderon.Location = new System.Drawing.Point(99, 296);
+            this.genderLabel_calderon.Name = "genderLabel_calderon";
+            this.genderLabel_calderon.Size = new System.Drawing.Size(81, 20);
+            this.genderLabel_calderon.TabIndex = 10;
+            this.genderLabel_calderon.Text = "Gender *";
+            // 
+            // birthLabel_calderon
+            // 
+            this.birthLabel_calderon.AutoSize = true;
+            this.birthLabel_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.birthLabel_calderon.Location = new System.Drawing.Point(99, 332);
+            this.birthLabel_calderon.Name = "birthLabel_calderon";
+            this.birthLabel_calderon.Size = new System.Drawing.Size(117, 20);
+            this.birthLabel_calderon.TabIndex = 11;
+            this.birthLabel_calderon.Text = "Date of birth*";
+            // 
+            // dayComboBox_calderon
+            // 
+            this.dayComboBox_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.dayComboBox_calderon.FormattingEnabled = true;
+            this.dayComboBox_calderon.Location = new System.Drawing.Point(103, 356);
+            this.dayComboBox_calderon.Name = "dayComboBox_calderon";
+            this.dayComboBox_calderon.Size = new System.Drawing.Size(77, 28);
+            this.dayComboBox_calderon.TabIndex = 12;
+            this.dayComboBox_calderon.Text = "-Day-";
+            // 
+            // monthComboBox_calderon
+            // 
+            this.monthComboBox_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.monthComboBox_calderon.FormattingEnabled = true;
+            this.monthComboBox_calderon.Location = new System.Drawing.Point(200, 356);
+            this.monthComboBox_calderon.Name = "monthComboBox_calderon";
+            this.monthComboBox_calderon.Size = new System.Drawing.Size(77, 28);
+            this.monthComboBox_calderon.TabIndex = 13;
+            this.monthComboBox_calderon.Text = "-Month-";
+            // 
+            // yearComboBox_calderon
+            // 
+            this.yearComboBox_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.yearComboBox_calderon.FormattingEnabled = true;
+            this.yearComboBox_calderon.Location = new System.Drawing.Point(294, 356);
+            this.yearComboBox_calderon.Name = "yearComboBox_calderon";
+            this.yearComboBox_calderon.Size = new System.Drawing.Size(77, 28);
+            this.yearComboBox_calderon.TabIndex = 14;
+            this.yearComboBox_calderon.Text = "-Year-";
+            // 
+            // clearButton_calderon
+            // 
+            this.clearButton_calderon.BackColor = System.Drawing.Color.Crimson;
+            this.clearButton_calderon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clearButton_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.clearButton_calderon.ForeColor = System.Drawing.Color.White;
+            this.clearButton_calderon.Location = new System.Drawing.Point(247, 497);
+            this.clearButton_calderon.Name = "clearButton_calderon";
+            this.clearButton_calderon.Size = new System.Drawing.Size(174, 36);
+            this.clearButton_calderon.TabIndex = 16;
+            this.clearButton_calderon.Text = "&Clear";
+            this.clearButton_calderon.UseVisualStyleBackColor = false;
+            this.clearButton_calderon.Click += new System.EventHandler(this.clearButton_calderon_Click);
+            // 
+            // exitButton_calderon
+            // 
+            this.exitButton_calderon.BackColor = System.Drawing.Color.Crimson;
+            this.exitButton_calderon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.exitButton_calderon.ForeColor = System.Drawing.Color.White;
+            this.exitButton_calderon.Location = new System.Drawing.Point(139, 539);
+            this.exitButton_calderon.Name = "exitButton_calderon";
+            this.exitButton_calderon.Size = new System.Drawing.Size(174, 36);
+            this.exitButton_calderon.TabIndex = 17;
+            this.exitButton_calderon.Text = "&Exit";
+            this.exitButton_calderon.UseVisualStyleBackColor = false;
+            this.exitButton_calderon.Click += new System.EventHandler(this.exitButton_calderon_Click);
+            // 
+            // registerButton_calderon
+            // 
+            this.registerButton_calderon.BackColor = System.Drawing.Color.Crimson;
+            this.registerButton_calderon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.registerButton_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.registerButton_calderon.ForeColor = System.Drawing.Color.White;
+            this.registerButton_calderon.Location = new System.Drawing.Point(26, 497);
+            this.registerButton_calderon.Name = "registerButton_calderon";
+            this.registerButton_calderon.Size = new System.Drawing.Size(174, 36);
+            this.registerButton_calderon.TabIndex = 18;
+            this.registerButton_calderon.Text = "&Register Student";
+            this.registerButton_calderon.UseVisualStyleBackColor = false;
+            this.registerButton_calderon.Click += new System.EventHandler(this.registerButton_calderon_Click);
+            // 
+            // headerTitle_calderon
+            // 
+            this.headerTitle_calderon.AutoSize = true;
+            this.headerTitle_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.headerTitle_calderon.Location = new System.Drawing.Point(110, 22);
+            this.headerTitle_calderon.Name = "headerTitle_calderon";
+            this.headerTitle_calderon.Size = new System.Drawing.Size(252, 24);
+            this.headerTitle_calderon.TabIndex = 19;
+            this.headerTitle_calderon.Text = "Student Registration Form";
+            // 
+            // progLabel_calderon
+            // 
+            this.progLabel_calderon.AutoSize = true;
+            this.progLabel_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.progLabel_calderon.Location = new System.Drawing.Point(99, 404);
+            this.progLabel_calderon.Name = "progLabel_calderon";
+            this.progLabel_calderon.Size = new System.Drawing.Size(151, 20);
+            this.progLabel_calderon.TabIndex = 20;
+            this.progLabel_calderon.Text = "Program to apply*";
+            // 
+            // programComboBox_calderon
+            // 
+            this.programComboBox_calderon.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.programComboBox_calderon.DropDownWidth = 335;
+            this.programComboBox_calderon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.programComboBox_calderon.FormattingEnabled = true;
+            this.programComboBox_calderon.Location = new System.Drawing.Point(94, 441);
+            this.programComboBox_calderon.Name = "programComboBox_calderon";
+            this.programComboBox_calderon.Size = new System.Drawing.Size(268, 28);
+            this.programComboBox_calderon.TabIndex = 22;
+            this.programComboBox_calderon.SelectedIndexChanged += new System.EventHandler(this.programComboBox_calderon_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(465, 612);
+            this.Controls.Add(this.programComboBox_calderon);
+            this.Controls.Add(this.progLabel_calderon);
+            this.Controls.Add(this.headerTitle_calderon);
+            this.Controls.Add(this.registerButton_calderon);
+            this.Controls.Add(this.exitButton_calderon);
+            this.Controls.Add(this.clearButton_calderon);
+            this.Controls.Add(this.yearComboBox_calderon);
+            this.Controls.Add(this.monthComboBox_calderon);
+            this.Controls.Add(this.dayComboBox_calderon);
+            this.Controls.Add(this.birthLabel_calderon);
+            this.Controls.Add(this.genderLabel_calderon);
+            this.Controls.Add(this.femaleSelect_calderon);
+            this.Controls.Add(this.maleSelect_calderon);
+            this.Controls.Add(this.middleNameInput_calderon);
+            this.Controls.Add(this.middleNameLabel_calderon);
+            this.Controls.Add(this.firstNameInput_calderon);
+            this.Controls.Add(this.firstNameLabel_calderon);
+            this.Controls.Add(this.lastNameInput_calderon);
+            this.Controls.Add(this.lastNameLabel_calderon);
+            this.Name = "Form1";
+            this.Text = "Student Registration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lastNameLabel_calderon;
+        private System.Windows.Forms.TextBox lastNameInput_calderon;
+        private System.Windows.Forms.TextBox firstNameInput_calderon;
+        private System.Windows.Forms.Label firstNameLabel_calderon;
+        private System.Windows.Forms.TextBox middleNameInput_calderon;
+        private System.Windows.Forms.Label middleNameLabel_calderon;
+        private System.Windows.Forms.RadioButton maleSelect_calderon;
+        private System.Windows.Forms.RadioButton femaleSelect_calderon;
+        private System.Windows.Forms.Label genderLabel_calderon;
+        private System.Windows.Forms.Label birthLabel_calderon;
+        private System.Windows.Forms.ComboBox dayComboBox_calderon;
+        private System.Windows.Forms.ComboBox monthComboBox_calderon;
+        private System.Windows.Forms.ComboBox yearComboBox_calderon;
+        private System.Windows.Forms.Button clearButton_calderon;
+        private System.Windows.Forms.Button exitButton_calderon;
+        private System.Windows.Forms.Button registerButton_calderon;
+        private System.Windows.Forms.Label headerTitle_calderon;
+        private System.Windows.Forms.Label progLabel_calderon;
+        private System.Windows.Forms.ComboBox programComboBox_calderon;
+    }
+}
+
---
+++ StudentRegistrationApplication/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentRegistrationApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += dayCombo_Load; //calls the method when starting the program
+            this.Load += monthCombo_Load;
+            this.Load += yearCombo_Load;
+            programComboBox_calderon_SelectedIndexChanged(null, null);
+
+        }
+
+        private void Form1_Load1(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void dayCombo_Load(object sender, EventArgs e) //method to initiate the loop when starting the program
+        {
+            for (int i = 1; i <= 31; i++) //loops the days (31)
+            {
+                dayComboBox_calderon.Items.Add(i);
+            }
+        }
+
+        private void monthCombo_Load(object sender, EventArgs e) //method to initiate the loop when starting the program
+        {
+            string[] months_calderon = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+            foreach (var month_calderon in months_calderon)
+            {
+                monthComboBox_calderon.Items.Add(month_calderon);
+            }
+        }
+
+        private void yearCombo_Load(object sender, EventArgs e) //method to initiate the loop when starting the program
+        {
+            for (int i = 1850; i <= 2024; i++) //loops the current year
+            {
+                yearComboBox_calderon.Items.Add(i);
+            }
+        }
+
+        private void registerButton_calderon_Click(object sender, EventArgs e)
+        {
+
+            String fName_calderon, mName_calderon, lName_calderon, gender_calderon;
+
+            fName_calderon = firstNameInput_calderon.Text;
+            mName_calderon = middleNameInput_calderon.Text;
+            lName_calderon = lastNameInput_calderon.Text;
+
+            if (maleSelect_calderon.Checked)
+            {
+                gender_calderon = " Male";
+            } 
+            else if (femaleSelect_calderon.Checked)
+            {
+                gender_calderon = "Female";
+            }
+            else
+            {
+                gender_calderon = "";
+            }
+
+            if (string.IsNullOrEmpty(fName_calderon) && string.IsNullOrEmpty(mName_calderon) && string.IsNullOrEmpty(lName_calderon))
+            {
+                MessageBox.Show("Please fill all the blanks.");
+            }
+            else if (string.IsNullOrEmpty(lName_calderon)) //if user did not input a last name
+            {
+                MessageBox.Show("You cannot leave the last name blank.");
+            }
+            else if (string.IsNullOrEmpty(fName_calderon)) //if user did not input a first name
+            {
+                MessageBox.Show("You cannot leave the first name blank.");
+            }
+            else if (string.IsNullOrEmpty(mName_calderon)) //if user did not input a middle name
+            {
+                MessageBox.Show("You cannot leave the middle name blank.");
+            }
+            else if (dayComboBox_calderon.SelectedIndex == 0) //if user did not select a day
+            {
+                MessageBox.Show("Please select a day.");
+            }
+            else if (monthComboBox_calderon.SelectedIndex == 0) //if user did not select a month
+            {
+                MessageBox.Show("Please select a month.");
+            }
+            else if (yearComboBox_calderon.SelectedIndex == 0) //if user did not select a year
+            {
+                MessageBox.Show("Please select a year.");
+            }
+            else if (!maleSelect_calderon.Checked && !femaleSelect_calderon.Checked) //if user did not select a gender
+            {
+                MessageBox.Show("Please select a gender");
+            }
+            else if (programComboBox_calderon.SelectedIndex == 0) // if user did not select a program
+
+            {
+                MessageBox.Show("Please select a program.");
+            }
+            else
+            {
+                MessageBox.Show("Student name: " + fName_calderon + " " + mName_calderon + " " + lName_calderon
+               + "\nGender: " + gender_calderon
+               + "\nDate of birth: " + dayComboBox_calderon.Text + "/" + monthComboBox_calderon.Text + "/" + yearComboBox_calderon.Text
+               + "\nProgram: " + programComboBox_calderon.Text);
+            }
+
+        }
+
+        private void clearButton_calderon_Click(object sender, EventArgs e)
+        {
+            firstNameInput_calderon.Text = "";
+            middleNameInput_calderon.Text = "";
+            lastNameInput_calderon.Text = "";
+            dayComboBox_calderon.Text = "-Day-";
+            monthComboBox_calderon.Text = "-Month-";
+            yearComboBox_calderon.Text = "-Year-";
+        }
+
+        private void exitButton_calderon_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void programComboBox_calderon_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ArrayList courseList_calderon = new ArrayList();
+            courseList_calderon.Add("-Select a program-");
+            courseList_calderon.Add("Bachelor of Science in Computer Science");
+            courseList_calderon.Add("Bachelor of Science in Information Technology");
+            courseList_calderon.Add("Bachelor of Science in Information System");
+            courseList_calderon.Add("Bachelor of Science in Computer Engineering");
+            foreach (var course_calderon in courseList_calderon)
+            {
+                programComboBox_calderon.Items.Add(course_calderon);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
